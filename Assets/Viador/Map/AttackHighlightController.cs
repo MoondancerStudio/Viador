6c573577cc5a0a57cb6af8d118f88fcd02513cd4
Balintskac <valentine_41@windowslive.com>
2025-03-24 23:10:05 +0100

    Refactored attack logic, attack action has an effect for number of round

--- Assets/Viador/Map/AttackHighlightController.cs
+++ Assets/Viador/Map/AttackHighlightController.cs
@@ -11,13 +11,14 @@ namespace Viador.Map
 
         [SerializeField] GameEvent selectAttackEvent;
         [SerializeField] LayerMask layerIncluded;
-
+        [SerializeField] private int threshold;
 
         private TilemapCollider2D _tilemapCollider;
         private Grid _grid;
 
         void Awake()
         {
+            threshold = threshold == 0 ? 1 : threshold;
             _grid = GameObject.Find("Grid").GetComponent<Grid>();
             _tilemapCollider = GetComponent<TilemapCollider2D>();
             _tilemapCollider.isTrigger = true;
@@ -25,13 +26,13 @@ namespace Viador.Map
 
         private void OnMouseDown()
         {
+            /*
             Vector2 mousePos = Input.mousePosition;
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
             worldPos.z = 0;
             Vector3Int tilePos = _grid.WorldToCell(worldPos);
-            //    Debug.Log($"Click on {tilePos}");
+            Debug.Log($"Click on {tilePos}");
 
-            /*
             if (Physics2D.OverlapBox(worldPos, sizeOfBoxCollider, 0, layerIncluded) is Collider2D targetHit)
             {
                 if (targetHit != null)
@@ -43,6 +44,19 @@ namespace Viador.Map
                 }
             }
             */
+        }
+
+        public void OnActionPointsUpdatedForAttack(Component sender, object actionPoints)
+        {
+            Debug.Log("ActionPointsUpdated for attack: " + actionPoints);
+            bool haveEnoughActionPoints = threshold <= (int)actionPoints;
+            EnableTilemapInteractionsForAttack(haveEnoughActionPoints);
+        }
+
+        private void EnableTilemapInteractionsForAttack(bool value)
+        {
+            GetComponent<TilemapRenderer>().enabled = value;
+            GetComponent<TilemapCollider2D>().enabled = value;
         }
     }
 }
